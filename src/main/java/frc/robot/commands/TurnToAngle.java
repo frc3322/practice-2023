442be6ac6209f6eb9f35e0f3a35a4b93d347af8e
Skyline Robotics <skylinerobotics@gmail.com>
2023-01-21 14:14:59 -0500

    Sysid stuff

--- src/main/java/frc/robot/commands/TurnToAngle.java
+++ src/main/java/frc/robot/commands/TurnToAngle.java
@@ -2,7 +2,6 @@
 // // Open Source Software; you can modify and/or share it under the terms of
 // // the WPILib BSD license file in the root directory of this project.
 
-<<<<<<< HEAD
 // package frc.robot.commands;
 
 // import edu.wpi.first.math.controller.PIDController;
@@ -29,45 +28,6 @@
 //         output -> drive.drive(0, output),
 //         // Require the drive
 //         drive);
-=======
-//unused
-
-package frc.robot.commands;
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
-import frc.robot.Constants.DriveConstants;
-import frc.robot.subsystems.Drivetrain;
-import io.github.oblarg.oblog.Loggable;
-import io.github.oblarg.oblog.annotations.Config;
-import io.github.oblarg.oblog.annotations.Log;
-
-/** A command that will turn the robot to the specified angle. */
-public class TurnToAngle extends PIDCommand implements Loggable{
-  /**
-   * Turns to robot to the specified angle.
-   *
-   * @param targetAngleDegrees The angle to turn to
-   * @param drive The drive subsystem to use
-   * 
-   */
-@Config static PIDController acontroller = new PIDController(0.02, 0, 0.001);
-
-
-
-  public TurnToAngle(double targetAngleDegrees, Drivetrain drive) {
-    super(
-
-      acontroller,
-        // Close loop on heading
-        drive::getYaw,
-        // Set reference to target
-        targetAngleDegrees,
-        // Pipe output to turn robot
-        output -> drive.drive(0, output),
-        // Require the drive
-        drive);
->>>>>>> 2ba30152a311f0b115492cc616d1b7c406896819
 
 //     // Set the controller to be continuous (because it is an angle controller)
 //     getController().enableContinuousInput(-180, 180);
