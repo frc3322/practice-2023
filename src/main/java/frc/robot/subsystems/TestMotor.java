f0108581234cab5a9d7dc512871cafec136c767a
Sean <you@example.com>
2023-01-28 13:24:39 -0500

    Added PID to test motor

--- src/main/java/frc/robot/subsystems/TestMotor.java
+++ src/main/java/frc/robot/subsystems/TestMotor.java
@@ -8,33 +8,59 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import io.github.oblarg.oblog.Loggable;
+import io.github.oblarg.oblog.annotations.Config;
 import io.github.oblarg.oblog.annotations.Log;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TestMotor extends SubsystemBase implements Loggable {
+  
   final CANSparkMax testMotor = new CANSparkMax(Constants.CAN.testMotor, MotorType.kBrushless);
 
   // Create Encoders
   private final RelativeEncoder testMotor_ENC = testMotor.getEncoder();
+
+  // Creates PID
+  public final PIDController encoderToPosition = new PIDController(0, 0, 0);
+
+  
 
   // Gets Encoder Value
   @Log double encoderPosition = 0; 
 
   /** Creates a new Testmotor. */
   public TestMotor() {
+    SmartDashboard.putData("Encoder To Position", getEncoderCommand(encoderToPosition.getSetpoint()));
+    SmartDashboard.putData("encoderToPosition", encoderToPosition);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    encoderPosition = testMotor_ENC.getPosition();
+    getEncoderPosition();
     
+  }
+
+  public double getEncoderPosition() {
+    encoderPosition = testMotor_ENC.getPosition();
+    return encoderPosition;
   }
 
   public void setPower(double power) {
     testMotor.set(power);
+  }
+
+  public PIDCommand getEncoderCommand(double setpoint){
+    
+    testMotor_ENC.setPosition(0);
+    PIDCommand  d =  new PIDCommand(encoderToPosition, this::getEncoderPosition, setpoint, output -> setPower(output) , this);
+    //d.getController().setTolerance(0.0000000);
+    d.getController().enableContinuousInput(-0.5, 0.5);
+    return d;
   }
 
 }
